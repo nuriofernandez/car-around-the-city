f927fbfa8d15882471c19f4aad3b27f612268c61
Nurio <personal@nurio.me>
2025-07-20 21:42:41 +0200

    Implemented FORWARD/REVERSE/BREAK operations

--- render/screens/game/movement.go
+++ render/screens/game/movement.go
@@ -1,32 +1,54 @@
 package game
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+const (
+	MAX_SPEED_FORWARD = 0.7
+	MAX_SPEED_REVERSE = -0.3
+)
 
 var carPosition = rl.NewVector3(0, 0, 0)
 
-var carEngine = true
-var speed = float32(0.1)
+var acceleration = float32(0.1)
 
 func Movement() {
 	EngineSim()
 	if rl.IsKeyDown(rl.KeyW) {
-		carEngine = true
-		if speed <= 1 && speed >= 0 {
-			speed += 0.1
-		}
+		acceleration = MaxSpeed(acceleration + 0.02)
 	}
 
 	if rl.IsKeyDown(rl.KeyS) {
-		speed -= 0.1
-		if speed <= 0.1 {
-			carEngine = false
-			speed = 0
+		acceleration = MaxSpeed(acceleration - 0.02)
+	}
+
+	if rl.IsKeyDown(rl.KeySpace) {
+		if acceleration < 0.1 && acceleration > -0.1 {
+			acceleration = 0
+		}
+		if acceleration < -0.1 { // REVERSE
+			acceleration = MaxSpeed(acceleration + 0.03)
+		}
+		if acceleration > 0.1 { // FORWARD
+			acceleration = MaxSpeed(acceleration - 0.03)
 		}
 	}
 }
 
 func EngineSim() {
-	if carEngine {
-		carPosition.Z += speed
+	if acceleration == 0 {
+		return
 	}
+	carPosition.Z += acceleration
+}
+
+func MaxSpeed(speed float32) float32 {
+	if speed > MAX_SPEED_FORWARD {
+		return MAX_SPEED_FORWARD
+	}
+	if speed < MAX_SPEED_REVERSE {
+		return MAX_SPEED_REVERSE
+	}
+	return speed
 }
